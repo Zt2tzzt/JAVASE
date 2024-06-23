9bed24dee81319f1518437d55b28c924cea73d89
zetian <zetian.zhu@foxmail.com>
2024-06-23 21:26:27 +0800

    feat: 02;

---
+++ demo-project/base-code/day02/src/com/kkcf/demo01/Haha.java
@@ -0,0 +1,11 @@
+package com.kkcf.demo01;
+
+import java.util.Scanner;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("哈哈");
+
+        new Scanner(System.in);
+    }
+}
