3c65cce19fa41a573395d87f7585fae73598c57a
zetian <zetian.zhu@foxmail.com>
2024-07-01 13:48:11 +0800

    fix: 03;

--- demo-project/base-code/Day08/src/com/kkcf/object_riented/Student.java
+++ demo-project/base-code/Day08/src/com/kkcf/object_riented/Student.java
@@ -2,10 +2,15 @@ package com.kkcf.object_riented;
 
 public class Student {
     private int age;
+    private char gender;
 
     public void method() {
         int age = 10;
         System.out.println(age);
         System.out.println(this.age);
+    }
+
+    public char getGender() {
+        return gender;
     }
 }
--- demo-project/base-code/Day08/src/com/kkcf/object_riented/Test03.java
+++ demo-project/base-code/Day08/src/com/kkcf/object_riented/Test03.java
@@ -3,6 +3,8 @@ package com.kkcf.object_riented;
 public class Test03 {
     public static void main(String[] args) {
         Student s = new Student();
-        s.method();
+        //s.method();
+
+        System.out.println("哈哈" + s.getGender() + "嘿嘿");
     }
 }
